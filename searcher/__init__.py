a8136418bdcde7208f8ecaabfdbdd06c4b4299ab
Felipe Rodrigues <felipe@felipevr.com>
2019-08-03 10:27:51 -0300

    Add Entrez's e-mail and API Key attributes to searcher initialization
    Those attributes are used for authentication and validation. They
    are set by Entrez as module-wide variables.
    
    This commit adds them to the Searcher initialization, that in turns
    sets them in Entrez.
    
    References:
    https://biopython.org/DIST/docs/api/Bio.Entrez-module.html#section-Variables

--- searcher/__init__.py
+++ searcher/__init__.py
@@ -1,10 +1,16 @@
+"""
+Supplies the main Searcher service
+"""
 from Bio import Entrez
 
+
 class Searcher:
-    
-    def __init__(self, db="sra"):
+    def __init__(self, email, api_key=None, db="sra"):
+        self.api_key = api_key
         self.db = db
+        self.email = email
+        Entrez.email = self.email
+        Entrez.api_key = self.api_key
 
     def search(self, query):
-        Entrez.email = "felipe@felipevr.com"
         Entrez.esearch(self.db, query, retmax=1)
--- searcher/tests/test_searcher.py
+++ searcher/tests/test_searcher.py
@@ -1,6 +1,7 @@
 """
 Unit tests for the searcher module
 """
+# pylint: disable=redefined-outer-name
 import pytest
 from Bio import Entrez
 from searcher import Searcher
@@ -8,19 +9,22 @@ from searcher import Searcher
 
 @pytest.fixture
 def searcher():
-    return Searcher()
+    """
+    Fixture that provides a Searcher instance
+    """
+    return Searcher("test@test.com")
 
 
-def test_searcher_initialization():
+def test_searcher_initialization(searcher):
     """
     Tests a searcher initialization parameters
     """
-    searcher = Searcher()
+
     assert searcher
     assert searcher.db == "sra"
 
-    searcher = Searcher(db="other_db")
-    assert searcher.db == "other_db"
+    new_searcher = Searcher("another@test.com", db="other_db")
+    assert new_searcher.db == "other_db"
 
 
 def test_searcher_searches_sra(searcher: Searcher, mocker):
@@ -28,7 +32,21 @@ def test_searcher_searches_sra(searcher: Searcher, mocker):
     Tests if the searcher, when supplied with a valid search string,
     calls the correct Biopython's Entrez methods
     """
-    
+
     mocker.patch("Bio.Entrez.esearch")
     searcher.search('"Homo sapiens"[Organism]')
-    Entrez.esearch.assert_called_with('sra', '"Homo sapiens"[Organism]', retmax=1)
+
+    # pylint: disable=no-member
+    Entrez.esearch.assert_called_with("sra", '"Homo sapiens"[Organism]', retmax=1)
+
+
+def test_searcher_configurer_entrez(mocker):
+    """
+    In order for everything to work, the Searcher must set Entrez's e-mail and
+    API Key parameters
+    """
+
+    Searcher(email="test@test.com", api_key="3141516")
+
+    assert Entrez.email == "test@test.com"
+    assert Entrez.api_key == "3141516"
