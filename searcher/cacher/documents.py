cdc46a8cc55f07c2469a11a24e3aed968116368c
Felipe Rodrigues <felipe@felipevr.com>
2019-08-11 19:21:36 -0300

    Add caching layer using mongodb

---
+++ searcher/cacher/documents.py
@@ -0,0 +1,24 @@
+"""
+Mongoengine documents used for caching
+"""
+# pylint: disable=wildcard-import, unused-wildcard-import
+from datetime import datetime
+
+from mongoengine import *
+from pytz import utc
+
+class SearchResult(DynamicDocument):
+    """
+    Mongoengine document to hold search results
+    """
+
+    created_at = DateTimeField(default=datetime.now(utc))
+
+class EntrezItem(DynamicDocument):
+    """
+    Mongoengine document to hold search items
+    """
+
+    created_at = DateTimeField(default=datetime.now(utc))
+    uid = StringField()
+    
